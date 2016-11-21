71bdedbd79f6f078be990a2c28fde5426d72b6b9
1301118 <1301118@rgu.ac.uk>
2016-11-21 14:52:57 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 1301118 on 21/11/2016.
+ */
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
